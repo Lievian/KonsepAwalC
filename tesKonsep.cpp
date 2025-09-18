e585d73b26a6a7b7a78eab92f875ef8a029b62a8
Lievian <rizalherudin8@gmail.com>
2025-09-19 00:59:08 +0700

    Add files via upload

---
+++ tesKonsep.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int pilihM;
+    long long saldo = 100000, deposit = 150000;
+    
+    
+    
+    cout << "\033[1;36m=========== \033[1;33m🔱BangKumis77🔱\033[0m \033[1;36m===========\033[0m\n";
+    cout << endl;
+    cout << "=====================================\n";
+    cout << "\033[1;33m🔱BangKumis77🔱\033[0m pasti rezeki!!🤑💸\n";
+    cout << "=====================================\n";
+    cout << left << setw(10) << "OVO✅" << setw(10) << "Dana✅" << setw(10) << "Gopay✅" << setw(10) << "BCA✅" << "BRI✅\n";
+    cout << "=====================================\n";
+    cout << "Halo \033[1;31mRizal\033[0m👋\n"; 
+    cout << "Selamat datang di \033[1;33m🔱BangKumis77🔱\033[0m\n";
+    cout << left << setw(15) << "Saldo"  << ": Rp" << saldo << endl;
+    cout << left << setw(15) << "Total deposit" << ": Rp" << deposit << endl;
+    cout << "=====================================\n";
+    cout << "1. Pilih angka🤘\n";
+    cout << "2. Withdraw💰\n";
+    cout << "3. Deposit💸\n";
+    cout << "4. Cek Win🔥\n";
+    cout << "=====================================\n";
+    cout << "\033[1;36mPilih: \033[0m";
+    cin >> pilihM;
+    if (pilihM==1) 
+    {
+        cout << "Coomingsoon";
+    } else 
+    {
+        cout << "Coomingsoon";
+    }
+    
+    return 0;
+}
